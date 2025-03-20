be93b4d9bcf23f88fcc74f7c802cde98fe1cedc3
abhijitjavelin <abhijit.hema@getjavelin.io>
2025-03-20 09:27:56 +0530

    fix: add latest langchain integrations

---
+++ examples/gemini/langchain_chatmodel_example.py
@@ -0,0 +1,11 @@
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("gemini-1.5-flash", model_provider="openai", base_url="https://api-dev.javelin.live/v1",
+extra_headers={"x-javelin-route": "google_univ", "x-api-key": os.environ.get("JAVELIN_API_KEY"), "Authorization": f"Bearer {os.environ.get('GEMINI_API_KEY')}"})
+
+print(model.invoke("write a poem about a cat"))
